e7482de2f2c7632b936d368ddc236a2b92b18f76
dltest <dongjia>
2020-06-08 14:46:45 +0800

    add nginx and uwsgi config

--- config.py
+++ config.py
@@ -70,8 +70,8 @@ class BaseConfig(object):
 
 
 class ProductionConfig(BaseConfig):
-    SECRET_KEY = os.getenv('EXAMPLE_API_SERVER_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('EXAMPLE_API_SERVER_SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = "verysecret" #os.getenv('EXAMPLE_API_SERVER_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = "sqlite://" #os.getenv('EXAMPLE_API_SERVER_SQLALCHEMY_DATABASE_URI')
 
 
 class DevelopmentConfig(BaseConfig):
